9105c697a62652ae6c90237ab9af2f92e3be6ae1
stephan-kashkarov <kashkarovwork@gmail.com>
2019-09-10 15:13:35 +1000

    added app class 2

---
+++ UI/app.py
@@ -0,0 +1,22 @@
+
+class App:
+
+    def __init__(self, word):
+        self.word = word
+
+
+    def draw(self):
+        pass
+
+    def interact(self):
+        pass
+
+    def update(self):
+        pass
+
+    def run(self):
+        while True:
+            self.draw()
+            self.interact()
+            self.update()
+
