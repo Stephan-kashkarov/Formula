6f52f33831e643d99a9a9e2a65ca29c142afe19c
stephan-kashkarov <kashkarovwork@gmail.com>
2019-09-10 15:41:09 +1000

    added clock app

---
+++ apps/clock.py
@@ -0,0 +1,14 @@
+from ui.app import App
+
+class DigitalClock(App):
+    def __init__(self):
+        super().__init__()
+    
+    def update(self):
+        pass
+    
+    def interact(self):
+        pass
+
+    def draw(self):
+        pass
